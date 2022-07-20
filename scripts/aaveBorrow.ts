25e26c56b06baf1336de13d92a33430be7c37456
ankitpal1029 <ankitkumarpal27@gmail.com>
2022-07-20 11:39:57 +0530

    deposit erc20 token

--- scripts/aaveBorrow.ts
+++ scripts/aaveBorrow.ts
@@ -1,6 +1,23 @@
+import { BigNumber } from "ethers";
 import { ethers, getNamedAccounts } from "hardhat";
 import { Address } from "hardhat-deploy/dist/types";
 import { getWeth } from "../scripts/getWeth";
+const AMOUNT = ethers.utils.parseEther("0.02");
+
+const approveERC20 = async (
+  erc20Address: Address,
+  spenderAddress: Address,
+  amountToSpend: BigNumber,
+  account: Address
+) => {
+  const erc20Token = await ethers.getContractAt(
+    "IERC20",
+    erc20Address,
+    account
+  );
+  const tx = await erc20Token.approve(spenderAddress, amountToSpend);
+  await tx.wait(1);
+};
 
 const getLendingPool = async (account: Address) => {
   const lendingPoolAddressesProvider = await ethers.getContractAt(
@@ -22,8 +39,23 @@ const main = async () => {
   await getWeth();
   const { deployer } = await getNamedAccounts();
   // Lending Pool Address provider: 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
-  const lendingPool = getLendingPool(deployer);
+  const lendingPool = await getLendingPool(deployer);
   console.log(`Lending Pool address ${(await lendingPool).address}`);
+
+  // deposit
+  const wethTokenAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+  // approve
+  await approveERC20(
+    wethTokenAddress,
+    (
+      await lendingPool
+    ).address,
+    AMOUNT,
+    deployer
+  );
+  console.log("Depositing...");
+  await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0);
+  console.log("Deposited!");
 };
 
 main()
